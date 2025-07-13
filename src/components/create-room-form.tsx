fb39d059a0f48bfb7a35989231d053bf6706f48e
Evandro-Cantelmo <evandrocantelmo@gmail.com>
2025-07-13 17:54:35 -0300

    feat: all audio and question implementations

---
+++ src/components/create-room-form.tsx
@@ -0,0 +1,91 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod/v4";
+import { UseCreateRoom } from "@/http/use-create-room";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+const createRoomSchema = z.object({
+  name: z.string().min(3, { message: "Inclua no mínimo 3 caracteres" }),
+  description: z.string(),
+});
+
+type CreateRoomFormData = z.infer<typeof createRoomSchema>;
+
+export function CreateRoomForm() {
+  const { mutateAsync: createRoom } = UseCreateRoom();
+
+  const createRoomForm = useForm<CreateRoomFormData>({
+    resolver: zodResolver(createRoomSchema),
+    defaultValues: { name: "", description: "" },
+  });
+
+  async function handleCreateRoom({ name, description }: CreateRoomFormData) {
+    await createRoom({ name, description });
+    createRoomForm.reset();
+  }
+  return (
+    <Card>
+      <CardTitle>Criar sala</CardTitle>
+      <CardDescription>
+        Crie uma nova sala para começar a fazer perguntas e receber respostas da
+        I.A.
+      </CardDescription>
+      <CardContent>
+        <Form {...createRoomForm}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={createRoomForm.handleSubmit(handleCreateRoom)}
+          >
+            <FormField
+              control={createRoomForm.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nome da sala</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Digite o nome da sala..."
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={createRoomForm.control}
+              name="description"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Descrição da sala</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <Button className="w-full" type="submit">
+              Criar sala
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/pages/record-room-audio.tsx
@@ -0,0 +1,105 @@
+import { useRef, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+type RoomParams = {
+  roomId: string;
+};
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === "function" &&
+  typeof window.MediaRecorder === "function";
+
+export function RecordRoomAudio() {
+  const [isRecording, setIsRecording] = useState(false);
+
+  const recorder = useRef<MediaRecorder | null>(null);
+  const params = useParams<RoomParams>();
+  const intervalRef = useRef<NodeJS.Timeout>(null);
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+
+    formData.append("file", audio, "audio.webm");
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const result = await response.json();
+
+    console.log(result);
+  }
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (recorder.current && recorder.current.state !== "inactive") {
+      recorder.current.stop();
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }
+
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: "audio/webm",
+      audioBitsPerSecond: 64_000,
+    });
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data);
+        // biome-ignore lint/suspicious/noConsole: <explanation>
+        console.log(event.data);
+      }
+    };
+    recorder.current.onstart = () => {
+      console.log("Gravaçao iniciada");
+    };
+    recorder.current.onstop = () => {
+      console.log("gravaçao encerrada");
+    };
+
+    recorder.current.start();
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert("O seu navegador não suporta gravação");
+      return;
+    }
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+    createRecorder(audio);
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+      createRecorder(audio);
+    }, 5000);
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+  return (
+    <div className=" flex h-screen flex-col items-center justify-center gap-3">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Parar áudio</Button>
+      ) : (
+        <Button onClick={startRecording}>Gravar áudio</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  );
+}
